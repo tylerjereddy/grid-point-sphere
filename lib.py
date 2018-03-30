9fd51e635bf444efed6dee842b97f08c8c33883b
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-03-30 16:35:27 -0600

    ENH: added parameter k to functions related to equation 1 from paper.

--- lib.py
+++ lib.py
@@ -4,13 +4,16 @@ algorithm implementation described by Li et al. (2017).
 
 import numpy as np
 
-def calc_m_lambda(i, j, N=0, MAXD=4):
+def calc_m_lambda(i, j, k=1.0, N=0, MAXD=4):
     '''
     From equation 1 in Li et al. (2017)
 
     i is the level of the grid
 
     j is the grid cell on the level designated by i
+
+    k is a coefficient empirically set to 1
+    by authors
 
     MAXD is the set maximum level of the
     multilevel grids
@@ -30,13 +33,16 @@ def calc_m_lambda(i, j, N=0, MAXD=4):
 
     return m_lambda
 
-def calc_m_phi(i, j, N=0, MAXD=4):
+def calc_m_phi(i, j, k=1.0, N=0, MAXD=4):
     '''
     From equation 1 in Li et al. (2017)
 
     i is the level of the grid
 
     j is the grid cell on the level designated by i
+
+    k is a coefficient empirically set to 1
+    by authors
 
     MAXD is the set maximum level of the
     multilevel grids
