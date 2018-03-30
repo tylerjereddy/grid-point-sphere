bdccb23c4a095742dc483b4627f1cd029afe5bff
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-03-30 16:31:24 -0600

    ENH: Added parameter N to functions related to equation 1 from paper.

--- lib.py
+++ lib.py
@@ -4,7 +4,7 @@ algorithm implementation described by Li et al. (2017).
 
 import numpy as np
 
-def calc_m_lambda(i, j, MAXD=4):
+def calc_m_lambda(i, j, N=0, MAXD=4):
     '''
     From equation 1 in Li et al. (2017)
 
@@ -14,6 +14,9 @@ def calc_m_lambda(i, j, MAXD=4):
 
     MAXD is the set maximum level of the
     multilevel grids
+
+    N is the number of spherical polygon
+    edges in the grid cell j at level i
 
     Returns m_lambda -- the number of cells for the
     grid in the latitude (N-S) direction.
@@ -27,7 +30,7 @@ def calc_m_lambda(i, j, MAXD=4):
 
     return m_lambda
 
-def calc_m_phi(i, j, MAXD=4):
+def calc_m_phi(i, j, N=0, MAXD=4):
     '''
     From equation 1 in Li et al. (2017)
 
@@ -37,6 +40,9 @@ def calc_m_phi(i, j, MAXD=4):
 
     MAXD is the set maximum level of the
     multilevel grids
+
+    N is the number of spherical polygon
+    edges in the grid cell j at level i
 
     Returns m_phi -- the number of cells for the
     grid in the longitude (E-W) direction.
