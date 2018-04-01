d07f11a5907533254f60be45f9ad2b1623435360
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-04-01 17:52:25 -0600

    ENH: Drafted new function to perform the evaluation described by Table 1 in the manuscript.

--- lib.py
+++ lib.py
@@ -85,3 +85,38 @@ def calc_m_phi(i, j, k=1.0, l_lambda=1, l_phi=1, N=0, MAXD=4):
 def _grid_build_coef(k=1.0, l_lambda=1, l_phi=1, N=0):
     # utility function needed by calc_m_lambda and calc_m_phi
     return np.sqrt((k * N) / (l_lambda * l_phi))
+
+def inclusion_property(center_1_property,
+                       intersection_count):
+    '''
+    Determine the spherical polygon inclusion property
+    (inside / outside of polygon) of center 2 based
+    on the known polygon inclusion property of center 1
+    and the number of spherical polygon edges crossed by
+    the minor arc connecting center 2 back to center 1.
+
+    Both center points are the centers of grid cells.
+
+    Based on Table 1 in the manuscript
+
+    center_1_property: the known spherical polygon inclusion
+    property of grid center point 1; should be either
+    'inside' or 'outside'
+
+    intersection_count: the number of spherical polygon
+    edges intersected when connecting the minor arc
+    between grid center 1 and grid center 2
+
+    Returns: the spherical polygon inclusion property
+    of grid center point 2
+    '''
+    if center_1_property == 'inside':
+        if intersection_count % 2 != 0:
+            return 'outside'
+        else:
+            return 'inside'
+    else:
+        if intersection_count % 2 != 0:
+            return 'inside'
+        else:
+            return 'outside'
