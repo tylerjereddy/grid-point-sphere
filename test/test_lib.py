0db6311f1238c054268bb43e11d75d8b478b7209
Tyler Reddy <tyler.je.reddy@gmail.com>
2018-04-18 16:49:48 -0600

    TST: drafted test for integer return by calc_m_lambda().

--- test/test_lib.py
+++ test/test_lib.py
@@ -82,3 +82,23 @@ def test_grid_level_1_edge_count_boundary():
     max_allowed = int(result.size / 2.)
 
     assert np.count_nonzero(result) < max_allowed
+
+@pytest.mark.parametrize("i, j, k, l_lambda, l_phi, N", [
+                         (2, 3, 1.0, 2.5, 2.5, 10),
+                         (4, 9, 1.0, 4.5, 9.5, 200),
+                         (4, 9, 1.0, 4.5, 9.5, 0),
+                         ])
+def test_calc_m_lambda(i, j, k, l_lambda, l_phi, N):
+    # since the manuscript defines m_lambda
+    # as the number of cells in the latitude
+    # direction, we should at least ensure
+    # that the result with reasonable
+    # input is always an integer number
+    # of cells
+    result = lib.calc_m_lambda(i=i,
+                               j=j,
+                               l_lambda=l_lambda,
+                               l_phi=l_phi,
+                               N=N)
+    print("result:", result)
+    assert isinstance(result, int)
